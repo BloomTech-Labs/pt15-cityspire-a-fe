8f4c7361df9b95aabab3805f02f371ec6e33c1ac
eaczechova <ewa.czech@hotmail.com>
2021-02-01 20:59:44 +0100

    add searchForm ui

--- src/components/common/FormButton.js
+++ src/components/common/FormButton.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const FormButton = props => {
   // notice we don't pass a click handler to this component. It's supposed to be used as a FormButton only.
   // You'd want to use an onSubmit on the Form Element itself to keep your forms organized.
   return (
-    <button
+    <Button
       disabled={props.isDisabled}
       className={props.classType || 'primary'}
     >
       {props.buttonText}
-    </button>
+    </Button>
   );
 };
 
@@ -21,3 +22,32 @@ FormButton.propTypes = {
   classType: PropTypes.string,
   disabled: PropTypes.string,
 };
+
+const Button = styled.button`
+  height: 32px;
+  padding: 4px 15px;
+  font-size: 14px;
+  border-radius: 2px;
+  color: #ec3944;
+  background: #fff;
+  border-color: #ec3944;
+  cursor: pointer;
+  height: 45px;
+  border: 0.5px solid #d9d9d9;
+  color: rgba(0, 0, 0, 0.65);
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  &:hover {
+    border: 0.5px solid #ec3944;
+    color: #ec3944;
+  }
+  @media (min-width: 768px) {
+    height: 52px;
+    margin-left: 5px;
+  }
+
+  &:focus {
+    background-color: #ec3944;
+    color: #fff;
+    outline: none;
+  }
+`;
