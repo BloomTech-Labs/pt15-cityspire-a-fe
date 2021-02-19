6112022b6c24fb85cf8d395ae6e54325942d332a
Matt Steele <msteele11101@gmail.com>
2021-02-18 23:29:10 -0800

    Feature/loginstylechanges (#10)
    * got Data to display on basic card
    
    * removed mapbox geocoder as its not needed
    
    * some quick changes to login widget

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -17,11 +17,11 @@ const LoginContainer = () => {
       },
       features: { registration: false },
       // turning this feature on allows your widget to use Okta for user registration
-      logo: 'path-to-your-logo',
+      logo: 'https://i.imgur.com/m01LhoB.png',
       // add your custom logo to your signing/register widget here.
       i18n: {
         en: {
-          'primaryauth.title': 'Welcome to Labs Basic SPA Please sign in',
+          'primaryauth.title': 'Welcome to City Spire Please sign in',
           // change title for your app
         },
       },
